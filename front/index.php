b7dfd0aa44152694e3b8d43d32a7a51bcfe2ef98
rami baccour <user_user007@yahoo.fr>
2021-12-26 07:37:57 +0100

    maj

--- front/index.php
+++ front/index.php
@@ -4,10 +4,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <link rel="stylesheet" href="css/main.css" />
+        <link rel="stylesheet" href="css/header.css" />
     </head>
     <body>
     <?php
-        include 'view/header.php';
+        include 'view/header/header.php';
     ?>
     </body>
 </html>
