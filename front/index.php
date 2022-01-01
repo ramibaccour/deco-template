db3fdd2462d18254a7f1880562cc76c59ee1035c
rami baccour <user_user007@yahoo.fr>
2022-01-01 07:37:16 +0100

    maj

--- front/index.php
+++ front/index.php
@@ -3,6 +3,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="stylesheet" href="css/bootstrap.min.css" />
+        <link rel="stylesheet" href="css/font-awesome.css" />
         <link rel="stylesheet" href="css/main.css" />
         <link rel="stylesheet" href="css/header.css" />
     </head>
@@ -10,5 +12,6 @@
     <?php
         include 'view/header/header.php';
     ?>
+    <script src='js/bootstrap.bundle.min.js'></script>
     </body>
 </html>
